0f4454f412fc930a811a73bbafff00975de6e11c
Markus <markuspetzke@gmail.com>
2025-05-28 21:37:15 +0200

    changed from continuous mode to oneshot mode

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use anyhow::Result;
+use esp_idf_hal::adc::oneshot::AdcDriver;
 use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_svc::log::EspLogger;
 use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition, wifi::EspWifi};
 use serde_json::json;
 use std::{thread::sleep, time::Duration};
@@ -7,19 +9,21 @@ use std::{thread::sleep, time::Duration};
 use embedded_svc::http::client::Client;
 use embedded_svc::io::Write;
 use embedded_svc::wifi::{ClientConfiguration, Configuration};
-use esp_idf_svc::hal::adc::{AdcContConfig, AdcContDriver, AdcMeasurement, Attenuated};
+use esp_idf_hal::adc::attenuation::DB_11;
+use esp_idf_hal::adc::oneshot::config::AdcChannelConfig;
+use esp_idf_hal::adc::oneshot::*;
 use esp_idf_svc::http::client::{Configuration as HttpConfig, EspHttpConnection};
-
-const CALIBRATION: f32 = 20.70;
+use log::info;
+const PH_SLOPE: f32 = -5.7;
+const ADC_REF_VOLTAGE: f32 = 3.3;
+const CALIBRATION: f32 = 21.00;
 
 fn main() -> Result<()> {
+    EspLogger::initialize_default();
     esp_idf_svc::sys::link_patches();
+
     let Peripherals {
-        pins,
-        modem,
-        adc1,
-        i2s0,
-        ..
+        pins, modem, adc1, ..
     } = Peripherals::take().unwrap();
 
     let sys_loop = EspSystemEventLoop::take().unwrap();
@@ -32,6 +36,7 @@ fn main() -> Result<()> {
         password: env!("SSID_PASSWORD").try_into().unwrap(),
         ..Default::default()
     });
+    info!("WIFI connected");
 
     wifi_driver.set_configuration(&ap_config).unwrap();
     wifi_driver.start().unwrap();
@@ -42,40 +47,43 @@ fn main() -> Result<()> {
         sleep(Duration::new(10, 0));
     }
 
-    let adc_pin = Attenuated::db11(pins.gpio34);
-    let mut adc = AdcContDriver::new(adc1, i2s0, &AdcContConfig::default(), adc_pin)?;
+    let adc = AdcDriver::new(adc1)?;
+    let config = AdcChannelConfig {
+        attenuation: DB_11,
+        ..Default::default()
+    };
+    let mut adc_pin = AdcChannelDriver::new(&adc, pins.gpio34, &config)?;
 
-    adc.start()?;
     loop {
-        let ph = {
-            let mut samples = [AdcMeasurement::default(); 10];
+        let mut samples = [0u16; 10];
+        for val in &mut samples {
+            *val = adc.read(&mut adc_pin)?;
+            sleep(Duration::from_millis(30));
+        }
+        samples.sort_unstable();
+        let avg: u32 = samples[2..8].iter().map(|&v| v as u32).sum::<u32>() / 6;
+        let corrected = avg as f32 / 0.597; // "simulierte Kalibrierung"
+        let voltage: f32 = corrected * ADC_REF_VOLTAGE / 4095.0;
 
-            if let Ok(num_read) = adc.read(&mut samples, 10) {
-                let mut raw_values: Vec<u16> =
-                    samples[..num_read].iter().map(|m| m.data()).collect();
+        let ph = CALIBRATION + PH_SLOPE * voltage;
 
-                raw_values.sort_unstable();
-                let avg: u32 = raw_values[2..8].iter().map(|&v| v as u32).sum();
-                let avg = avg as f32 / 6.0;
-                let voltage = avg * 3.3 / 4095.0;
-                -5.70 * voltage + CALIBRATION
-            } else {
-                0.0
-            }
-        };
+        info!("ADC: {} -> {:.3} V -> pH = {:.2}", avg, voltage, ph);
 
-        let httpconnection = EspHttpConnection::new(&HttpConfig::default())?;
-        let body = json!({"ph_value": ph, "sensor_id": env!("ID")}).to_string();
-        let headers = &[
-            ("Content-Type", "application/json"),
-            ("Content-Length", &body.len().to_string()),
-        ];
+        if (0.0..=14.0).contains(&ph) {
+            let httpconnection = EspHttpConnection::new(&HttpConfig::default())?;
+            let body =
+                json!({    "ph_value": format!("{:.3}", ph), "sensor_id": env!("ID")}).to_string();
+            let headers = &[
+                ("Content-Type", "application/json"),
+                ("Content-Length", &body.len().to_string()),
+            ];
 
-        let mut httpclient = Client::wrap(httpconnection);
-        let mut request = httpclient.post(env!("URL"), headers)?;
-        request.write_all(body.as_bytes())?;
-        request.submit()?;
+            let mut httpclient = Client::wrap(httpconnection);
+            let mut request = httpclient.post(env!("URL"), headers)?;
+            request.write_all(body.as_bytes())?;
+            request.submit()?;
+        }
 
-        sleep(Duration::new(120, 0));
+        sleep(Duration::new(30, 0));
     }
 }
